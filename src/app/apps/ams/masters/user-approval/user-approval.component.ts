891265f04587719cffbeb7456ea5fc1b150d7f44
Roop Sagarh Talari <talariroopsagar@gmail.com>
2023-01-12 17:51:33 +0530

    minor changes

--- src/app/apps/ams/masters/user-approval/user-approval.component.ts
+++ src/app/apps/ams/masters/user-approval/user-approval.component.ts
@@ -8,10 +8,11 @@ import { UserService } from 'src/app/shared/moduleservices/user.service';
   styleUrls: ['./user-approval.component.scss']
 })
 export class UserApprovalComponent implements OnInit {
-  data:any = [ ];
-  groupedData={};
+  data: any = [];
+  groupedData = {};
   Object = Object;
   payload = [];
+  isLoading: boolean = true;
 
   constructor(
     private userService: UserService,
@@ -21,11 +22,11 @@ export class UserApprovalComponent implements OnInit {
   ngOnInit(): void {
     this.getUsers();
   }
-  getUsers(){
-    this.userService.getAllUsers().subscribe((res:any) => {
+  getUsers() {
+    this.userService.getAllUsers().subscribe((res: any) => {
       this.data = res.filter(elem => elem.department_id != null);
-  this.groupedData = this.groupBy(this.data, 'department_id');
-	console.log(this.groupedData);
+      this.groupedData = this.groupBy(this.data, 'department_id');
+      this.isLoading = false;
 
     })
   }
@@ -35,28 +36,28 @@ export class UserApprovalComponent implements OnInit {
   //   })
   // }
   groupBy(xs, key) {
-    return xs.reduce(function(rv, x) {
+    return xs.reduce(function (rv, x) {
       (rv[x[key]] = rv[x[key]] || []).push(x);
       return rv;
     }, {});
   };
-  selected(item){
+  selected(item) {
     let count = 0;
-    let itemIndex:number;
-    this.payload.forEach((elem:any,index:any) => {
-      if(elem.department_name === item) {
+    let itemIndex: number = undefined;
+    this.payload.forEach((elem: any, index: any) => {
+      if (elem.department_name === item) {
         count++;
         itemIndex = index;
       }
     })
-    if(count === 0) this.payload.push({
-      reason:'',
-      status:'pending',
+    if (count === 0) this.payload.push({
+      reason: '',
+      status: 'pending',
       department_name: item
     })
-    if(count > 0) this.payload.splice(itemIndex,1);
+    if (count > 0) this.payload.splice(itemIndex, 1);
   }
-  submit(){
+  submit() {
     console.log(this.payload);
   }
 }
